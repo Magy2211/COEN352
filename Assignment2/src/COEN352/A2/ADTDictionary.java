ab5cf51515cafa8820e9b293c6aec362a1bd1fae
pixhawkstanaccount <59042749+chrisirakiza@users.noreply.github.com>
2022-10-25 21:23:45 -0400

    added a new function to the ADT dictionnary

--- Assignment2/src/COEN352/A2/ADTDictionary.java
+++ Assignment2/src/COEN352/A2/ADTDictionary.java
@@ -24,6 +24,12 @@ public interface ADTDictionary<K, E> {
       "k", remove an arbitrary one. Return null if no record
       with key "k" exists. */
   public E remove(K k);
+  
+  /** adding a new function to create an index. 
+   * the function takes the attribute as input and return the element 
+   */ 
+  
+  public E[] createIndex(String attribute); 
 
   /** Remove and return an arbitrary record from dictionary.
       @return the record removed, or null if none exists. */
