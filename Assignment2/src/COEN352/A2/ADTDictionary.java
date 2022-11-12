1ec312523a54ddba651942e72b1833416f0eaf00
Magy2211 <115485610+Magy2211@users.noreply.github.com>
2022-11-12 13:52:23 -0500

    Add files via upload

--- Assignment2/src/COEN352/A2/ADTDictionary.java
+++ Assignment2/src/COEN352/A2/ADTDictionary.java
@@ -1,38 +1,46 @@
+package coen352.ch5.bitree;
+
+
+
 /** Source code example for "A Practical Introduction to Data
-    Structures and Algorithm Analysis, 3rd Edition (Java)" 
-    by Clifford A. Shaffer
-    Copyright 2008-2011 by Clifford A. Shaffer
+Structures and Algorithm Analysis, 3rd Edition (Java)" 
+by Clifford A. Shaffer
+Copyright 2008-2011 by Clifford A. Shaffer
 */
 
 /** The Dictionary abstract class. */
+public interface ADTDictionary<Key, E> {
 
-package COEN352.A2; 
+/** Reinitialize dictionary */
+public void clear();
 
-public interface ADTDictionary<K, E> {
+/** Insert a record
+  @param k The key for the record being inserted.
+  @param e The record being inserted. */
+public void insert(Key k, E e);
 
-  /** Reinitialize dictionary */
-  public void clear();
+/** Remove and return a record.
+  @param k The key of the record to be removed.
+  @return A maching record. If multiple records match
+  "k", remove an arbitrary one. Return null if no record
+  with key "k" exists. */
+public E remove(Key k);
 
-  /** Insert a record
-      @param  k The key for the record being inserted.
-      @param  e The record being inserted. */
-  public void insert(K k, E e);
+/** Remove and return an arbitrary record from dictionary.
+  @return the record removed, or null if none exists. */
+public E removeAny();
 
-  /** Remove and return a record.
-      @param k The key of the record to be removed.
-      @return A matching record. If multiple records match
-      "k", remove an arbitrary one. Return null if no record
-      with key "k" exists. */
-  public E remove(K k);
+/** @return A record matching "k" (null if none exists).
+  If multiple records match, return an arbitrary one.
+  @param k The key of the record to find */
+public E find(Key k);
 
-  /** Remove and return an arbitrary record from dictionary.
-      @return the record removed, or null if none exists. */
-  public E removeAny();
+/** @return The number of records in the dictionary. */
+public int size();
 
-  /** @return A record matching "k" (null if none exists).
-      If multiple records match, return an arbitrary one. */
-  public E find(K k);
 
-  /** @return the number of records in the dictionary. */
-  public int size();
+
+
+	
+
 };
