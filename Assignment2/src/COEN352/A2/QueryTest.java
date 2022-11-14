46e8445e414a79787397b51347802694cf4a466a
pixhawkstanaccount <59042749+chrisirakiza@users.noreply.github.com>
2022-11-14 18:25:07 -0500

    making changes to better reflect assignment 2 requirements

--- Assignment2/src/COEN352/A2/QueryTest.java
+++ Assignment2/src/COEN352/A2/QueryTest.java
@@ -15,171 +15,39 @@ public class QueryTest {
 			warehouseDb = new Warehouse_DB_Linked_List();
 			
 			// First item 
-			
-			Inventory inventory_1 = new Inventory(); 
-			
-			inventory_1.setSKU("IN0001"); 
-			inventory_1.setName("Item 1");
-			inventory_1.setDescription("Desc 1");
-			inventory_1.setUnitPrice(51.00);
-			inventory_1.setQuantityInStock(25);
-			inventory_1.setInventoryValue(1275.00);
-			inventory_1.setReorderQuantity(29);
-			inventory_1.setReorderTime(13);
-			inventory_1.setQuantityInReorder(50);
-			inventory_1.setDiscontinuedStatus(true);
+			Inventory inventory_1 = new Inventory("IN0001","Item 1","Desc 1",51.00,25,1275.00,29,13,50,true); 
 			
 			// Second item 
-			
-			Inventory inventory_2 = new Inventory(); 
-			
-			inventory_2.setSKU("IN0002"); 
-			inventory_2.setName("Item 2");
-			inventory_2.setDescription("Desc 2");
-			inventory_2.setUnitPrice(93.00);
-			inventory_2.setQuantityInStock(132);
-			inventory_2.setInventoryValue(12276.00);
-			inventory_2.setReorderQuantity(231);
-			inventory_2.setReorderTime(4);
-			inventory_2.setQuantityInReorder(50);
-			inventory_2.setDiscontinuedStatus(true);
+			Inventory inventory_2 = new Inventory("IN0002","Item 2","Desc 2",93.00,132,12276.00,231,4,50,true); 
 			
 			// Third item 
-			
-			Inventory inventory_3 = new Inventory(); 
-			
-			inventory_3.setSKU("IN0003"); 
-			inventory_3.setName("Item 3");
-			inventory_3.setDescription("Desc 3");
-			inventory_3.setUnitPrice(57.00);
-			inventory_3.setQuantityInStock(151);
-			inventory_3.setInventoryValue(8607.00);
-			inventory_3.setReorderQuantity(114);
-			inventory_3.setReorderTime(11);
-			inventory_3.setQuantityInReorder(150);
-			inventory_3.setDiscontinuedStatus(false);
+			Inventory inventory_3 = new Inventory("IN0003","Item 3","Desc 3",57.00,151,8607.00,114,11,150,false); 
 			
 			// Fourth item 
+			Inventory inventory_4 = new Inventory("IN0004","Item 4","Desc 4",19.00,186,3534.00,158,6,50,false); 
 			
-			Inventory inventory_4 = new Inventory(); 
-			
-			inventory_4.setSKU("IN0004"); 
-			inventory_4.setName("Item 4");
-			inventory_4.setDescription("Desc 4");
-			inventory_4.setUnitPrice(19.00);
-			inventory_4.setQuantityInStock(186);
-			inventory_4.setInventoryValue(3534.00);
-			inventory_4.setReorderQuantity(158);
-			inventory_4.setReorderTime(6);
-			inventory_1.setQuantityInReorder(50);
-			inventory_1.setDiscontinuedStatus(false);
-			
-			// Fifth item 
-			
-			Inventory inventory_5 = new Inventory(); 
-			
-			inventory_5.setSKU("IN0005"); 
-			inventory_5.setName("Item 5");
-			inventory_5.setDescription("Desc 5");
-			inventory_5.setUnitPrice(75.00);
-			inventory_5.setQuantityInStock(62);
-			inventory_5.setInventoryValue(4650.00);
-			inventory_5.setReorderQuantity(39);
-			inventory_5.setReorderTime(12);
-			inventory_5.setQuantityInReorder(50);
-			inventory_5.setDiscontinuedStatus(false);
-			
+			// Fifth item
+			Inventory inventory_5 = new Inventory("IN004","Item 5","Desc 5",75.00,62,4650.00,39,12,50,false); 
 			
 			// Sixth item 
-			
-			Inventory inventory_6 = new Inventory(); 
-			
-			inventory_6.setSKU("IN0006"); 
-			inventory_6.setName("Item 6");
-			inventory_6.setDescription("Desc 6");
-			inventory_6.setUnitPrice(11.00);
-			inventory_6.setQuantityInStock(5);
-			inventory_6.setInventoryValue(55.00);
-			inventory_6.setReorderQuantity(9);
-			inventory_6.setReorderTime(13);
-			inventory_6.setQuantityInReorder(150);
-			inventory_6.setDiscontinuedStatus(true);
+			Inventory inventory_6 = new Inventory("IN0006","Item 6","Desc 6",11.00,5,55.00,9,13,150,false); 
 			
 			// Seventh item 
-			
-			Inventory inventory_7 = new Inventory(); 
-			
-			inventory_7.setSKU("IN0007"); 
-			inventory_7.setName("Item 7");
-			inventory_7.setDescription("Desc 7");
-			inventory_7.setUnitPrice(56.00);
-			inventory_7.setQuantityInStock(58);
-			inventory_7.setInventoryValue(3248.00);
-			inventory_7.setReorderQuantity(109);
-			inventory_7.setReorderTime(7);
-			inventory_7.setQuantityInReorder(100);
-			inventory_7.setDiscontinuedStatus(false);
+			Inventory inventory_7 = new Inventory("IN0007","Item 7","Desc 7",56.00,58,3428.00,109,7,100,false); 
 			
 			// Eighth item 
-			
-			Inventory inventory_8 = new Inventory(); 
-			
-			inventory_8.setSKU("IN0008"); 
-			inventory_8.setName("Item 8");
-			inventory_8.setDescription("Desc 8");
-			inventory_8.setUnitPrice(38.00);
-			inventory_8.setQuantityInStock(101);
-			inventory_8.setInventoryValue(3838.00);
-			inventory_8.setReorderQuantity(162);
-			inventory_8.setReorderTime(3);
-			inventory_8.setQuantityInReorder(100);
-			inventory_8.setDiscontinuedStatus(true);;
+			Inventory inventory_8 = new Inventory("IN0008","Item 8","Desc 8",38.00,101,3838.00,162,3,100,true); 
 			
 			// ninth item 
-			
-			Inventory inventory_9 = new Inventory(); 
-			
-			inventory_9.setSKU("IN0009"); 
-			inventory_9.setName("Item 9");
-			inventory_9.setDescription("Desc 9");
-			inventory_9.setUnitPrice(59.00);
-			inventory_9.setQuantityInStock(122);
-			inventory_9.setInventoryValue(7198.00);
-			inventory_9.setReorderQuantity(82);
-			inventory_9.setReorderTime(3);
-			inventory_9.setQuantityInReorder(150);
-			inventory_9.setDiscontinuedStatus(false);
+			Inventory inventory_9 = new Inventory("IN0009","Item 9","Desc 9",59.00,122,7198.00,82,3,150,false); 
 			
 			// tenth item 
-			
-			Inventory inventory_10 = new Inventory(); 
-			
-			inventory_10.setSKU("IN0010"); 
-			inventory_10.setName("Item 10");
-			inventory_10.setDescription("Desc 10");
-			inventory_10.setUnitPrice(50.00);
-			inventory_10.setQuantityInStock(175);
-			inventory_10.setInventoryValue(8750.00);
-			inventory_10.setReorderQuantity(283);
-			inventory_10.setReorderTime(8);
-			inventory_10.setQuantityInReorder(150);
-			inventory_10.setDiscontinuedStatus(true);
-			
+			Inventory inventory_10 = new Inventory("IN0010","Item 10","Desc 10",50.00,175,8750.00,283,8,150,true); 
+		
 			// eleventh item 
-			
-			Inventory inventory_11 = new Inventory(); 
-			
-			inventory_11.setSKU("IN0011"); 
-			inventory_11.setName("Item 11");
-			inventory_11.setDescription("Desc 11");
-			inventory_11.setUnitPrice(59.00);
-			inventory_11.setQuantityInStock(176);
-			inventory_11.setInventoryValue(10384.00);
-			inventory_11.setReorderQuantity(229);
-			inventory_11.setReorderTime(1);
-			inventory_11.setQuantityInReorder(100);
-			inventory_11.setDiscontinuedStatus(true);
-			
+			Inventory inventory_11 = new Inventory("IN0011","Item 11","Desc 11",59.00,176,10384.00,229,1,100,true); 
+		
+			// inserting records in the database 
 			warehouseDb.InsertRecord(inventory_11);
 			warehouseDb.InsertRecord(inventory_10);
 			warehouseDb.InsertRecord(inventory_9);
@@ -212,7 +80,7 @@ public class QueryTest {
 			
 			String returnedInv2 = warehouseDb.query("Unit Price", 0.37);
 			
-			assertEquals(TestElement2.PrintRecord(),returnedInv2," values are not in ascending order."); 
+			assertEquals(TestElement2.PrintRecord(),returnedInv2," Unit Price values are not in ascending order."); 
 		}
 	
 }
--- Assignment2/src/COEN352/A2/Warehouse_DB_Linked_List.java
+++ Assignment2/src/COEN352/A2/Warehouse_DB_Linked_List.java
@@ -1,296 +1,151 @@
 // Irakiza Dushime Chris 
 // Magy Gerges 
-// Assignment 2: implementing the warehouse class to execute the inventory functions 
-
+// Assignment 2: implementing the warehouse class  
 
 package COEN352.A2;
 import java.util.Arrays;
 import java.lang.Math;
 
-
-
 public class Warehouse_DB_Linked_List 
-
 {
-
-	public static void main(String[] args) 
-	
+public static void main(String[] args) 
 	{
-		
 		// declaring an object of the type warehouse 
 		
 		Warehouse_DB_Linked_List warehouseDb = new Warehouse_DB_Linked_List();
 		
-		// adding the entries from the excel sheet 
-		
-		// declaring an object of the type inventory to store all information using the set functions to populate the fields
-		
+			/* adding the entries from the excel sheet 
+			declaring an object of the type inventory to store all information using the set functions to populate the fields
+			*/
 				// First item 
-				
-				Inventory inventory_1 = new Inventory(); 
-				
-				inventory_1.setSKU("IN0001"); 
-				inventory_1.setName("Item 1");
-				inventory_1.setDescription("Desc 1");
-				inventory_1.setUnitPrice(51.00);
-				inventory_1.setQuantityInStock(25);
-				inventory_1.setInventoryValue(1275.00);
-				inventory_1.setReorderQuantity(29);
-				inventory_1.setReorderTime(13);
-				inventory_1.setQuantityInReorder(50);
-				inventory_1.setDiscontinuedStatus(true);
+				Inventory inventory_1 = new Inventory("IN0001","Item 01","Desc 01",51.00,25,1275.00,29,13,50,true); 
 				
 				// Second item 
-				
-				Inventory inventory_2 = new Inventory(); 
-				
-				inventory_2.setSKU("IN0002"); 
-				inventory_2.setName("Item 2");
-				inventory_2.setDescription("Desc 2");
-				inventory_2.setUnitPrice(93.00);
-				inventory_2.setQuantityInStock(132);
-				inventory_2.setInventoryValue(12276.00);
-				inventory_2.setReorderQuantity(231);
-				inventory_2.setReorderTime(4);
-				inventory_2.setQuantityInReorder(50);
-				inventory_2.setDiscontinuedStatus(true);
+				Inventory inventory_2 = new Inventory("IN0002","Item 02","Desc 02",93.00,132,12276.00,231,4,50,true); 
 				
 				// Third item 
-				
-				Inventory inventory_3 = new Inventory(); 
-				
-				inventory_3.setSKU("IN0003"); 
-				inventory_3.setName("Item 3");
-				inventory_3.setDescription("Desc 3");
-				inventory_3.setUnitPrice(57.00);
-				inventory_3.setQuantityInStock(151);
-				inventory_3.setInventoryValue(8607.00);
-				inventory_3.setReorderQuantity(114);
-				inventory_3.setReorderTime(11);
-				inventory_3.setQuantityInReorder(150);
-				inventory_3.setDiscontinuedStatus(false);
+				Inventory inventory_3 = new Inventory("IN0003","Item 03","Desc 03",57.00,151,8607.00,114,11,150,false); 
 				
 				// Fourth item 
+				Inventory inventory_4 = new Inventory("IN0004","Item 04","Desc 04",19.00,186,3534.00,158,6,50,false); 
 				
-				Inventory inventory_4 = new Inventory(); 
-				
-				inventory_4.setSKU("IN0004"); 
-				inventory_4.setName("Item 4");
-				inventory_4.setDescription("Desc 4");
-				inventory_4.setUnitPrice(19.00);
-				inventory_4.setQuantityInStock(186);
-				inventory_4.setInventoryValue(3534.00);
-				inventory_4.setReorderQuantity(158);
-				inventory_4.setReorderTime(6);
-				inventory_1.setQuantityInReorder(50);
-				inventory_1.setDiscontinuedStatus(false);
-				
-				// Fifth item 
-				
-				Inventory inventory_5 = new Inventory(); 
-				
-				inventory_5.setSKU("IN0005"); 
-				inventory_5.setName("Item 5");
-				inventory_5.setDescription("Desc 5");
-				inventory_5.setUnitPrice(75.00);
-				inventory_5.setQuantityInStock(62);
-				inventory_5.setInventoryValue(4650.00);
-				inventory_5.setReorderQuantity(39);
-				inventory_5.setReorderTime(12);
-				inventory_5.setQuantityInReorder(50);
-				inventory_5.setDiscontinuedStatus(false);
-				
+				// Fifth item
+				Inventory inventory_5 = new Inventory("IN0005","Item 05","Desc 05",75.00,62,4650.00,39,12,50,false); 
 				
 				// Sixth item 
-				
-				Inventory inventory_6 = new Inventory(); 
-				
-				inventory_6.setSKU("IN0006"); 
-				inventory_6.setName("Item 6");
-				inventory_6.setDescription("Desc 6");
-				inventory_6.setUnitPrice(11.00);
-				inventory_6.setQuantityInStock(5);
-				inventory_6.setInventoryValue(55.00);
-				inventory_6.setReorderQuantity(9);
-				inventory_6.setReorderTime(13);
-				inventory_6.setQuantityInReorder(150);
-				inventory_6.setDiscontinuedStatus(true);
+				Inventory inventory_6 = new Inventory("IN0006","Item 06","Desc 06",11.00,5,55.00,9,13,150,false); 
 				
 				// Seventh item 
-				
-				Inventory inventory_7 = new Inventory(); 
-				
-				inventory_7.setSKU("IN0007"); 
-				inventory_7.setName("Item 7");
-				inventory_7.setDescription("Desc 7");
-				inventory_7.setUnitPrice(56.00);
-				inventory_7.setQuantityInStock(58);
-				inventory_7.setInventoryValue(3248.00);
-				inventory_7.setReorderQuantity(109);
-				inventory_7.setReorderTime(7);
-				inventory_7.setQuantityInReorder(100);
-				inventory_7.setDiscontinuedStatus(false);
+				Inventory inventory_7 = new Inventory("IN0007","Item 07","Desc 07",56.00,58,3428.00,109,7,100,false); 
 				
 				// Eighth item 
-				
-				Inventory inventory_8 = new Inventory(); 
-				
-				inventory_8.setSKU("IN0008"); 
-				inventory_8.setName("Item 8");
-				inventory_8.setDescription("Desc 8");
-				inventory_8.setUnitPrice(38.00);
-				inventory_8.setQuantityInStock(101);
-				inventory_8.setInventoryValue(3838.00);
-				inventory_8.setReorderQuantity(162);
-				inventory_8.setReorderTime(3);
-				inventory_8.setQuantityInReorder(100);
-				inventory_8.setDiscontinuedStatus(true);;
+				Inventory inventory_8 = new Inventory("IN0008","Item 08","Desc 08",38.00,101,3838.00,162,3,100,true); 
 				
 				// ninth item 
-				
-				Inventory inventory_9 = new Inventory(); 
-				
-				inventory_9.setSKU("IN0009"); 
-				inventory_9.setName("Item 9");
-				inventory_9.setDescription("Desc 9");
-				inventory_9.setUnitPrice(59.00);
-				inventory_9.setQuantityInStock(122);
-				inventory_9.setInventoryValue(7198.00);
-				inventory_9.setReorderQuantity(82);
-				inventory_9.setReorderTime(3);
-				inventory_9.setQuantityInReorder(150);
-				inventory_9.setDiscontinuedStatus(false);
+				Inventory inventory_9 = new Inventory("IN0009","Item 09","Desc 09",59.00,122,7198.00,82,3,150,false); 
 				
 				// tenth item 
-				
-				Inventory inventory_10 = new Inventory(); 
-				
-				inventory_10.setSKU("IN0010"); 
-				inventory_10.setName("Item 10");
-				inventory_10.setDescription("Desc 10");
-				inventory_10.setUnitPrice(50.00);
-				inventory_10.setQuantityInStock(175);
-				inventory_10.setInventoryValue(8750.00);
-				inventory_10.setReorderQuantity(283);
-				inventory_10.setReorderTime(8);
-				inventory_10.setQuantityInReorder(150);
-				inventory_10.setDiscontinuedStatus(true);
-				
+				Inventory inventory_10 = new Inventory("IN0010","Item 10","Desc 10",50.00,175,8750.00,283,8,150,true); 
+			
 				// eleventh item 
-				
-				Inventory inventory_11 = new Inventory(); 
-				
-				inventory_11.setSKU("IN0011"); 
-				inventory_11.setName("Item 11");
-				inventory_11.setDescription("Desc 11");
-				inventory_11.setUnitPrice(59.00);
-				inventory_11.setQuantityInStock(176);
-				inventory_11.setInventoryValue(10384.00);
-				inventory_11.setReorderQuantity(229);
-				inventory_11.setReorderTime(1);
-				inventory_11.setQuantityInReorder(100);
-				inventory_11.setDiscontinuedStatus(true);
+				Inventory inventory_11 = new Inventory("IN0011","Item 11","Desc 11",59.00,176,10384.00,229,1,100,true); 
 				
 				// twelfth item
+				Inventory inventory_12 = new Inventory("IN0012","Item 12","Desc 12",18.00,22,396.00,36,12,50,true); 
 				
-				Inventory inventory_12 = new Inventory(); 
+			// testing 
 				
-				inventory_12.setSKU("IN0012"); 
-				inventory_12.setName("Item 12");
-				inventory_12.setDescription("Desc 12");
-				inventory_12.setUnitPrice(18.00);
-				inventory_12.setQuantityInStock(22);
-				inventory_12.setInventoryValue(396.00);
-				inventory_12.setReorderQuantity(36);
-				inventory_12.setReorderTime(12);
-				inventory_12.setQuantityInReorder(50);
-				inventory_12.setDiscontinuedStatus(true);
-				
-				
-				// Testing the insert function 
 				System.out.println("-------------------------------------------------------------------------- ");
 				System.out.println(" Inserting new values: ...");
-				warehouseDb.InsertRecord(inventory_1);
 				warehouseDb.InsertRecord(inventory_2);
-				warehouseDb.InsertRecord(inventory_3);
 				warehouseDb.InsertRecord(inventory_4);
+				warehouseDb.InsertRecord(inventory_1);
+				warehouseDb.InsertRecord(inventory_7);
 				warehouseDb.InsertRecord(inventory_5);
 				warehouseDb.InsertRecord(inventory_6);
-				warehouseDb.InsertRecord(inventory_7);
+				warehouseDb.InsertRecord(inventory_3);
 				warehouseDb.InsertRecord(inventory_8);
+				warehouseDb.InsertRecord(inventory_10);
+				warehouseDb.InsertRecord(inventory_12);
 				warehouseDb.InsertRecord(inventory_9);
-				warehouseDb.InsertRecord(inventory_10);
 				warehouseDb.InsertRecord(inventory_11);
-				System.out.println("-------------------------------------------------------------------------- ");
 				
-				/* Verifying the total number and value of the added items in the database 
-				System.out.println(" Results: ");
-				System.out.println(" There are " + warehouseDb.TotalInventoryQuantity() + " inventories in this database.");
-				System.out.println(" The total value of the inventories is: " + warehouseDb.TotalInventoryValue());     
-				System.out.println();
-				
-				// printing the sorted array of the indicated index (which is the inventory member function)
-				
-				// System.out.println(Arrays.toString(warehouseDb.CreateIndex("Quantity")));
-				
-				// Testing if the insert function functions 
+				// Question 1.1: Testing the Createindex() method that return Inventory
 				System.out.println("-------------------------------------------------------------------------- ");
-				System.out.println(" Inserting " + inventory_10.getSKU() + " in the inventory database.... "); 
-				warehouseDb.InsertRecord(inventory_10);
 				System.out.println("-------------------------------------------------------------------------- ");
-				System.out.println(" Results: ");
-				// verifying if the totals have changed with the new input 
-				System.out.println(" There are " + warehouseDb.TotalInventoryQuantity() + " inventories in this database.");
-				System.out.println(" The total value of the inventories is: " + warehouseDb.TotalInventoryValue());
-				System.out.println();
-				
-				System.out.println("-------------------------------------------------------------------------- ");
-				// Testing the find function 
-				System.out.println(" Finding " + inventory_9.getSKU()+ " in the inventory database...");
-				System.out.println("-------------------------------------------------------------------------- ");
-				warehouseDb.findRecord("IN009");
-				System.out.println();
-
-				System.out.println("-------------------------------------------------------------------------- ");
-				System.out.println(" Removing " + inventory_10.getSKU()+ " From the inventory database...");
-				System.out.println("-------------------------------------------------------------------------- ");
-				// Testing the remove record and verifying if the totals have reduced 
-				warehouseDb.RemoveRecord("IN0010");
-				System.out.println(" There are " + warehouseDb.TotalInventoryQuantity() + " inventories in this database.");
-				System.out.println(" The total value of the inventories is: " + warehouseDb.TotalInventoryValue());
-				System.out.println();
-				
-				System.out.println("-------------------------------------------------------------------------- ");
-				// Testing the clear database function and verifying if the warehouse is clean 
-				System.out.println(" Clearing the inventory database .....  ");
-				warehouseDb.clearDatabase();
-				System.out.println("-------------------------------------------------------------------------- ");
-				
-				System.out.println(" Results: ");
-				System.out.println(" Trying to find "+ inventory_1.getSKU()+ " in the deleted inventory database...");
-				warehouseDb.findRecord("IN001");
-				System.out.println(" There are " + warehouseDb.TotalInventoryQuantity() + " inventories in this database.");
-				System.out.println(" The total value of the inventories is: " + warehouseDb.TotalInventoryValue());
-				*/
-				
-				System.out.println("-------------------------------------------------------------------------- ");
-				System.out.println("Testing the CreateIndex function....");
-				
-				Inventory [] sortedInventory = new Inventory[10];
-				sortedInventory = warehouseDb.CreateIndex("SKU"); 
+				System.out.println(" Question 1.1: Testing the CreateIndex function that outputs the sorted values of an attribute....");
+	
+				Inventory [] sortedInventory = new Inventory[11];
+				sortedInventory = warehouseDb.CreateIndex("Inventory Value"); 
 				
 				int i;
 				for(i=0; i < sortedInventory.length ; i++) 
 				{
 					System.out.print(sortedInventory[i].PrintRecord());
 				}
+				System.out.println();
+				System.out.println();
 				
-				BSTNode<Inventory> Inv = warehouseDb.createIndex("InventoryValue");
-
+				// Question 1.2: Testing the Createindex() method that does in-order traversing
+				System.out.println("-------------------------------------------------------------------------- ");
+				System.out.println("-------------------------------------------------------------------------- ");
+				System.out.println(" Question 1.2: Testing the CreateIndex function to return in-order traversing....");
+				System.out.println(); 
 				
-				warehouseDb.InOrderTraversing(Inv);
+				System.out.println("SKU");
+				BSTNode<Inventory> Inv1 = warehouseDb.createIndex("SKU");
+				warehouseDb.InOrderTraversing(Inv1);
+				System.out.println(); 
+				System.out.println("-------------------------------------------------------------------------- ");
 				
+				System.out.println(" Name");
+				BSTNode<Inventory> Inv2 = warehouseDb.createIndex("Name");
+				warehouseDb.InOrderTraversing(Inv2);
+				System.out.println(); 
+				System.out.println("-------------------------------------------------------------------------- ");
 				
+				System.out.println(" Description");
+				BSTNode<Inventory> Inv3 = warehouseDb.createIndex("Description");
+				warehouseDb.InOrderTraversing(Inv3);
+				System.out.println(); 
+				System.out.println("-------------------------------------------------------------------------- ");
+				
+				System.out.println(" Unit Price");
+				BSTNode<Inventory> Inv4 = warehouseDb.createIndex("Unit Price");
+				warehouseDb.InOrderTraversing(Inv4);
+				System.out.println();
+				System.out.println("-------------------------------------------------------------------------- ");
+				
+				System.out.println(" Quantity in Stock");
+				BSTNode<Inventory> Inv5 = warehouseDb.createIndex("Quantity in stock");
+				warehouseDb.InOrderTraversing(Inv5);
+				System.out.println(); 
+				System.out.println("-------------------------------------------------------------------------- ");
+				
+				System.out.println(" Inventory Value");
+				BSTNode<Inventory> Inv6 = warehouseDb.createIndex("Inventory Value");
+				warehouseDb.InOrderTraversing(Inv6);
+				System.out.println();
+				System.out.println("-------------------------------------------------------------------------- ");
+				
+				System.out.println(" Reorder Quantity");
+				BSTNode<Inventory> Inv7 = warehouseDb.createIndex("Reorder Quantity");
+				warehouseDb.InOrderTraversing(Inv7);
+				System.out.println();
+				System.out.println("-------------------------------------------------------------------------- ");
+				
+				System.out.println(" Reorder Time");
+				BSTNode<Inventory> Inv8 = warehouseDb.createIndex("Reorder Time");
+				warehouseDb.InOrderTraversing(Inv8);
+				System.out.println();
+				System.out.println("-------------------------------------------------------------------------- ");
+				
+				System.out.println(" Quantity in Reorder");
+				BSTNode<Inventory> Inv9 = warehouseDb.createIndex("Quantity in Reorder");
+				warehouseDb.InOrderTraversing(Inv9);
+				System.out.println();
+				
+				// Question 2 and Question 3. Queury() method is being tested in J-UNIT testing 
 	}
 	
 	// creating the database Variables 
@@ -298,7 +153,6 @@ public class Warehouse_DB_Linked_List
 	private double inventoryTotalValue; // will help in calculating total cost of the inventory
 	private int inventory_number_of_entries; // will help in calculating total value of the inventory
 	private Linked_List_Dictionary<String,Inventory> warehouse;  // warehouse object to store all inventory items
-	
 
 	// Database member function 
 	
@@ -477,17 +331,17 @@ public class Warehouse_DB_Linked_List
 		for(int i=0; i<=CopyInv.length-1; i++) {
 			
 			inserthelp(Attribute,TreeRoot,CopyInv[i]);
-			
 
 		}
 		return TreeRoot;
 	}
 	
+	//Q1.2
 	public void InOrderTraversing(BSTNode<Inventory> I) {
 		
 		if (I == null) return; // Empty subtree - do nothing
 		InOrderTraversing(I.left()); // Process left nodes
-		System.out.print(I.element().PrintRecord()); // Process root node
+		System.out.println(I.element().PrintRecord()); // Process root node
 		InOrderTraversing(I.right()); // Process all nodes in right
 		
 	}
@@ -684,8 +538,7 @@ public class Warehouse_DB_Linked_List
 						  inserthelp(Attribute,rt.left(),e);
 					  
 			  }
-			  else System.out.println (" Invalid attribute, please enter a valid attribute.");
-			  
+			  else System.out.println (" Invalid attribute, please enter a valid attribute."); 
 		  }
 		}
 	
