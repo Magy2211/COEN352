a90d9be32b1bb90052093aa20b7782c4f47126d6
Magym <Magym@DESKTOP-CE1SMJ8.wireless.concordia.ca>
2022-11-14 14:15:26 -0500

    Assignment done

--- Assignment2/src/COEN352/A2/BinNode.java
+++ Assignment2/src/COEN352/A2/BinNode.java
@@ -1,8 +1,8 @@
 /** Source code example for "A Practical Introduction to Data
     Structures and Algorithm Analysis, 3rd Edition (Java)" 
     by Clifford A. Shaffer
-    Copyright 2008-2011 by Clifford A. Shaffer
-*/
+    Copyright 2008-2011 by Clifford A. Shaffer 
+    */
 
 package COEN352.A2; 
 
